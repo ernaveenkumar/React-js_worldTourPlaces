c520573c2f42a89ce68ba0a38ce4e349e526fb66
Naveen Kumar <er.crackerjack@gmail.com>
2024-12-04 12:51:25 +1300

    cities context component created and used to get cities detial on card . Make Eslint error display by adding 'react/prop-types': 'off', in rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
-import PageNotFound from './pages/PageNotFound';
-import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
 import CityList from './pages/components/CityList';
 import CountryList from './pages/components/CountryList';
 import City from './pages/components/City';
 import Form from './pages/components/Form';
-
-const BASE_URL = 'http://localhost:3000';
+import { CitiesProvider } from './contexts/CitiesContext';
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  //load date right on mount
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert('There was an error loading data...');
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <>
-      <h1>Hello Router</h1>
-
+    <CitiesProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
@@ -48,23 +25,17 @@ function App() {
           <Route path="app" element={<AppLayout />}>
             {/* <Route index element={<p>List!!index element!!</p>} /> */}
             <Route index element={<Navigate replace to="cities" />} />
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route path="cities" element={<CityList />} />
 
             <Route path="cities/:id" element={<City />} />
 
-            <Route
-              path="countries"
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
+            <Route path="countries" element={<CountryList />} />
             <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </CitiesProvider>
   );
 }
 export default App;
