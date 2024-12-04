09785e078470350a33d5a6b4d0e866bb94b59c1a
Naveen Kumar <er.crackerjack@gmail.com>
2024-12-05 11:21:02 +1300

    On sigle city clicked or selected the  city information and map is displayed. working fine

--- src/pages/components/Map.jsx
+++ src/pages/components/Map.jsx
@@ -1,24 +1,38 @@
 /* eslint-disable react/prop-types */
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+// import { useState } from 'react';
 function Map() {
-  const navigate = useNavigate();
   // eslint-disable-next-line
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  // const lat = searchParams.get('lat');
+  // const lng = searchParams.get('lng');
+  // const [mapPostion, setMapPosition] = useState([40, 0]);
+  // function onCLickHandler() {
+  //   // setMapPosition([]);
+  // }
+  const mapPostion = [51.505, -0.09];
+
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
-    >
-      Map
-      <h1>
-        {lat} and {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPostion}
+        zoom={6}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPostion}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
